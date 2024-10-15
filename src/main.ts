bac757150c34b96fd5d6a40a4d48c679ef233796
Benjamin Edlinger <bedlinger@student.tgm.ac.at>
2024-10-15 21:24:52 +0200

    Install primevue and setup autoimport

--- src/main.ts
+++ src/main.ts
@@ -4,8 +4,16 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
 import './index.css'
+import PrimeVue from 'primevue/config'
+import Aura from '@primevue/themes/aura'
 
 const app = createApp(App)
+
+app.use(PrimeVue, {
+    theme: {
+        preset: Aura
+    }
+})
 
 app.use(router)
 
