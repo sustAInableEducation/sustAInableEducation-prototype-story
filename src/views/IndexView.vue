aed2ff6c45ebca59ea2993988263f11c5353b521
Benjamin Edlinger <bedlinger@student.tgm.ac.at>
2024-10-15 23:21:15 +0200

    Reformat

--- src/views/IndexView.vue
+++ src/views/IndexView.vue
@@ -1,12 +1,17 @@
 <script setup lang="ts">
 const btnClicked = (text: string) => {
-  alert(`Button clicked: ${text}`);
-};
+  alert(`Button clicked: ${text}`)
+}
 </script>
 
 <template>
   <div class="flex flex-col items-center">
     <h1 class="mt-3 mb-8 text-3xl underline">Hello world!</h1>
-    <Button class="w-28" label="Check" icon="pi pi-check" @click="btnClicked('Check')" />
+    <Button
+      class="w-28"
+      label="Check"
+      icon="pi pi-check"
+      @click="btnClicked('Check')"
+    />
   </div>
 </template>
