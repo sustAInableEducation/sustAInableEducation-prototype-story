cd3e8711d087921c25ea210fed38b6867823c9d4
Benjamin Edlinger <bedlinger@student.tgm.ac.at>
2024-10-20 22:55:36 +0200

    Rerformat and lint

--- src/views/StoryView.vue
+++ src/views/StoryView.vue
@@ -2,11 +2,11 @@
 import { stories } from '@/data/stories'
 
 defineProps({
-  id: Number
+  id: Number,
 })
 </script>
 
 <template>
-    <h1 v-if="id !== undefined">{{ stories[id].title }}</h1>
-    <h1 v-else>Story not found</h1>
+  <h1 v-if="id !== undefined">{{ stories[id].title }}</h1>
+  <h1 v-else>Story not found</h1>
 </template>
