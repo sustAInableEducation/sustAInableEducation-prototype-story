866c3d4308600b980c85e56bb738d806924b534c
Benjamin Edlinger <bedlinger@student.tgm.ac.at>
2024-10-20 22:47:38 +0200

    Outsource types and data

--- src/views/IndexView.vue
+++ src/views/IndexView.vue
@@ -1,40 +1,8 @@
 <script setup lang="ts">
-interface Story {
-  id: number
-  title: string
-  prompt: string
-}
-
-const stories: Story[] = [
-  {
-    id: 0,
-    title: 'Sauberes Wasser',
-    prompt:
-      'Schreibe eine Geschichte zum Thema "Sauberes Wasser". Diese Geschichte soll interaktiv sein, das bedeutet es wird insgesamt drei Entscheidungspunkte geben, jeder Entscheidungspunkt enthält vier Option, welchen jeweils einen unterschiedlichen Einfluss auf den weiteren Verlauf der Geschichte haben. Nach jedem Entscheidungspunkt stoppe und warte auf die gewählte Option. Jede Gute Geschichte brauch einen Titel, also bitte erstellen auch einen. Nach dem dritten Entscheidungspunkt soll die Geschichte beendet sein.',
-  },
-  {
-    id: 1,
-    title: 'Klimaschutzmaßnahmen',
-    prompt:
-      'Schreibe eine Geschichte zum Thema "Klimaschutzmaßnahmen". Diese Geschichte soll interaktiv sein, das bedeutet es wird insgesamt drei Entscheidungspunkte geben, jeder Entscheidungspunkt enthält vier Option, welchen jeweils einen unterschiedlichen Einfluss auf den weiteren Verlauf der Geschichte haben. Nach jedem Entscheidungspunkt stoppe und warte auf die gewählte Option. Jede Gute Geschichte brauch einen Titel, also bitte erstellen auch einen. Nach dem dritten Entscheidungspunkt soll die Geschichte beendet sein',
-  },
-  {
-    id: 2,
-    title: 'Nachhaltige Städte',
-    prompt:
-      'Schreibe eine Geschichte zum Thema "Nachhaltige Städte". Diese Geschichte soll interaktiv sein, das bedeutet es wird insgesamt drei Entscheidungspunkte geben, jeder Entscheidungspunkt enthält vier Option, welchen jeweils einen unterschiedlichen Einfluss auf den weiteren Verlauf der Geschichte haben. Nach jedem Entscheidungspunkt stoppe und warte auf die gewählte Option. Jede Gute Geschichte brauch einen Titel, also bitte erstellen auch einen. Nach dem dritten Entscheidungspunkt soll die Geschichte beendet sein.',
-  },
-  {
-    id: 3,
-    title: 'Sauberer Strom',
-    prompt:
-      'Schreibe eine Geschichte zum Thema "Sauberer Strom". Diese Geschichte soll interaktiv sein, das bedeutet es wird insgesamt drei Entscheidungspunkte geben, jeder Entscheidungspunkt enthält vier Option, welchen jeweils einen unterschiedlichen Einfluss auf den weiteren Verlauf der Geschichte haben. Nach jedem Entscheidungspunkt stoppe und warte auf die gewählte Option. Jede Gute Geschichte brauch einen Titel, also bitte erstellen auch einen. Nach dem dritten Entscheidungspunkt soll die Geschichte beendet sein.',
-  },
-]
+import { stories } from '@/data/stories'
 
 const startStory = (id: number) => {
-  console.log('Start Story', id)
-  // Navigate to story view
+  console.log('Start story with id:', id)
 }
 </script>
 
