3d43470a52c7e94b76c4b03e96d48622a5c97837
Benjamin Edlinger <bedlinger@student.tgm.ac.at>
2024-11-03 12:28:16 +0100

    Edit prompts

--- src/data/stories.ts
+++ src/data/stories.ts
@@ -5,24 +5,24 @@ export const stories: Story[] = [
     id: 0,
     title: 'Sauberes Wasser',
     prompt:
-      'Schreibe eine Geschichte zum Thema "Sauberes Wasser". Diese Geschichte soll interaktiv sein, das bedeutet es wird insgesamt drei Entscheidungspunkte geben, jeder Entscheidungspunkt enthält vier Option, welchen jeweils einen unterschiedlichen Einfluss auf den weiteren Verlauf der Geschichte haben. Nach jedem Entscheidungspunkt stoppe und warte auf die gewählte Option. Jede Gute Geschichte brauch einen Titel, also bitte erstellen auch einen. Nach dem dritten Entscheidungspunkt soll die Geschichte beendet sein.',
+      'Schreibe eine Geschichte zum Thema "Sauberes Wasser". Diese Geschichte soll interaktiv sein, das bedeutet es wird insgesamt drei Entscheidungspunkte geben, jeder Entscheidungspunkt enthält vier Option, welchen jeweils einen unterschiedlichen Einfluss auf den weiteren Verlauf der Geschichte haben. Nach jedem Entscheidungspunkt stoppe und warte auf die gewählte Option. Jede Gute Geschichte brauch einen Titel, also bitte erstellen auch einen. Nach dem dritten Entscheidungspunkt soll die Geschichte beendet sein. Sende deine Antworten im JSON Format.',
   },
   {
     id: 1,
     title: 'Klimaschutzmaßnahmen',
     prompt:
-      'Schreibe eine Geschichte zum Thema "Klimaschutzmaßnahmen". Diese Geschichte soll interaktiv sein, das bedeutet es wird insgesamt drei Entscheidungspunkte geben, jeder Entscheidungspunkt enthält vier Option, welchen jeweils einen unterschiedlichen Einfluss auf den weiteren Verlauf der Geschichte haben. Nach jedem Entscheidungspunkt stoppe und warte auf die gewählte Option. Jede Gute Geschichte brauch einen Titel, also bitte erstellen auch einen. Nach dem dritten Entscheidungspunkt soll die Geschichte beendet sein.',
+      'Schreibe eine Geschichte zum Thema "Klimaschutzmaßnahmen". Diese Geschichte soll interaktiv sein, das bedeutet es wird insgesamt drei Entscheidungspunkte geben, jeder Entscheidungspunkt enthält vier Option, welchen jeweils einen unterschiedlichen Einfluss auf den weiteren Verlauf der Geschichte haben. Nach jedem Entscheidungspunkt stoppe und warte auf die gewählte Option. Jede Gute Geschichte brauch einen Titel, also bitte erstellen auch einen. Nach dem dritten Entscheidungspunkt soll die Geschichte beendet sein. Sende deine Antworten im JSON Format.',
   },
   {
     id: 2,
     title: 'Nachhaltige Städte',
     prompt:
-      'Schreibe eine Geschichte zum Thema "Nachhaltige Städte". Diese Geschichte soll interaktiv sein, das bedeutet es wird insgesamt drei Entscheidungspunkte geben, jeder Entscheidungspunkt enthält vier Option, welchen jeweils einen unterschiedlichen Einfluss auf den weiteren Verlauf der Geschichte haben. Nach jedem Entscheidungspunkt stoppe und warte auf die gewählte Option. Jede Gute Geschichte brauch einen Titel, also bitte erstellen auch einen. Nach dem dritten Entscheidungspunkt soll die Geschichte beendet sein.',
+      'Schreibe eine Geschichte zum Thema "Nachhaltige Städte". Diese Geschichte soll interaktiv sein, das bedeutet es wird insgesamt drei Entscheidungspunkte geben, jeder Entscheidungspunkt enthält vier Option, welchen jeweils einen unterschiedlichen Einfluss auf den weiteren Verlauf der Geschichte haben. Nach jedem Entscheidungspunkt stoppe und warte auf die gewählte Option. Jede Gute Geschichte brauch einen Titel, also bitte erstellen auch einen. Nach dem dritten Entscheidungspunkt soll die Geschichte beendet sein. Sende deine Antworten im JSON Format.',
   },
   {
     id: 3,
     title: 'Sauberer Strom',
     prompt:
-      'Schreibe eine Geschichte zum Thema "Sauberer Strom". Diese Geschichte soll interaktiv sein, das bedeutet es wird insgesamt drei Entscheidungspunkte geben, jeder Entscheidungspunkt enthält vier Option, welchen jeweils einen unterschiedlichen Einfluss auf den weiteren Verlauf der Geschichte haben. Nach jedem Entscheidungspunkt stoppe und warte auf die gewählte Option. Jede Gute Geschichte brauch einen Titel, also bitte erstellen auch einen. Nach dem dritten Entscheidungspunkt soll die Geschichte beendet sein.',
+      'Schreibe eine Geschichte zum Thema "Sauberer Strom". Diese Geschichte soll interaktiv sein, das bedeutet es wird insgesamt drei Entscheidungspunkte geben, jeder Entscheidungspunkt enthält vier Option, welchen jeweils einen unterschiedlichen Einfluss auf den weiteren Verlauf der Geschichte haben. Nach jedem Entscheidungspunkt stoppe und warte auf die gewählte Option. Jede Gute Geschichte brauch einen Titel, also bitte erstellen auch einen. Nach dem dritten Entscheidungspunkt soll die Geschichte beendet sein. Sende deine Antworten im JSON Format.',
   },
 ]
